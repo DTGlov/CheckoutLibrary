b8767cbd9e566a88f68c2a52114fc1555f0608e2
DTGlov <dtglover21@gmail.com>
2023-05-02 19:11:34 +0000

    fix app not compiling

--- app/src/main/java/com/example/checkoutlibrary/MainActivity.kt
+++ app/src/main/java/com/example/checkoutlibrary/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.checkoutlibrary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.checkoutlibrary.ui.checkout.CheckoutScreen
 import com.example.checkoutlibrary.ui.theme.CheckTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +11,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             CheckTheme {
                 // A surface container using the 'background' color from the theme
-                CheckoutScreen()
             }
         }
     }
